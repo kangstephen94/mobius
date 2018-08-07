b9990c4143b57749bcb4207e94e4bc37c7f0b56f
Stephen Kang <kang.stephen94@gmail.com>
2018-08-07 16:17:42 -0700

    Hide keys

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -7,8 +7,8 @@ Rails.application.configure do
   address:              'smtp.gmail.com',
   port:                 587,
   domain:               'gmail.com',
-  user_name:            ENV['email'],
-  password:             ENV['password'],
+  user_name:            'skcoder94@gmail.com',
+  password:             'Hk2598538',
   authentication:       :login,
   enable_starttls_auto: true
 }
